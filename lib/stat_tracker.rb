39a390620bc2c51ae1b24446db88a0e058e04504
jaharaclark <jaharamclark@gmail.com>
2021-02-24 07:04:29 -1000

    Got 1 Working method, has pseduo code

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,37 @@
+require 'CSV'
+
 class StatTracker
   def self.from_csv(locations)
-    require 'CSV'
     CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |row|
       # id = row[:id].to_i
       away_team_id = row[:away_team_id].to_i
-      require 'pry'; binding.pry
+      # require 'pry'; binding.pry
     end
   end
+
+  def self.count_of_teams(locations)
+    counter = 0
+    CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |row|
+      counter += 1
+    end
+    puts counter
+    return counter
+  end
+  
 end
+
+# make a module for later
+
+
+# need an analytic class
+# create a bunch of methods utilizing the data from that class 
+# important distinction? stat Tracker lives at the top of the hierarchy
+# traversing the parallels (everything will know about itself) will need to get into the stat tracker and then stat tracker will reach into the other things
+
+  
+# def best_season(team_id)
+# games - sortby - season
+# end
+
+# best_season(3)
+
--- runner.rb
+++ runner.rb
@@ -10,6 +10,7 @@ locations = {
   game_teams: game_teams_path
 }
 
-stat_tracker = StatTracker.from_csv(locations)
+# stat_tracker = StatTracker.from_csv(locations)
+count_of_teams = StatTracker.count_of_teams(locations)
 
-require 'pry'; binding.pry
+# require 'pry'; binding.pry
